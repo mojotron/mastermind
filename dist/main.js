1c55a37124bdca367e9eeccdde2d1e7c00fbebd6
mojotron <igormalj@gmail.com>
2021-09-18 16:35:37 +0200

    Set up webpack and add basic game design

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/***/ (() => {
+
+eval("\"use script\";\n//Easy game 3 pins from 5 colors\n//Normal game set 4 pins from 6 colors\n//Hard mode 5 pins from 8 colors\n\n//colors\n//1) blue - #1ecbe1\n//2) green - #15ea48\n//3) yellow - #e1ec13\n//4) red - #f1200e\n//5) orange - #fa6805\n//6) purple - #ee11a4\n//7) brown - #6d2e09\n//8) black - #373338\n\n//fonts\n//<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n//<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n//<link href=\"https://fonts.googleapis.com/css2?family=Expletus+Sans&family=Gruppo&display=swap\" rel=\"stylesheet\">\n\n\n//# sourceURL=webpack://mastermind/./src/js/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+"use script";
+//Easy game 3 pins from 5 colors
+//Normal game set 4 pins from 6 colors
+//Hard mode 5 pins from 8 colors
+
+//number of guesses 10 or 12 depending on manufacturer
+
+//colors
+//1) blue - #1ecbe1
+//2) green - #15ea48
+//3) yellow - #e1ec13
+//4) red - #f1200e
+//5) orange - #fa6805
+//6) purple - #ee11a4
+//7) brown - #6d2e09
+//8) black - #373338
+//Flag colors
+//1) white - #ddd7fc
+//2) red - #f2080b
+
+//fonts
+//<link rel="preconnect" href="https://fonts.googleapis.com">
+//<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+//<link href="https://fonts.googleapis.com/css2?family=Expletus+Sans&family=Gruppo&display=swap" rel="stylesheet">
+
+//make number of random colors
+//wait for user to input colors
+//compare
+//return result
